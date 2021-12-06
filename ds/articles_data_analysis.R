cb285a3dabb0d27b49e7446896ec6a6415f9136b
timschott <tcs9pk@virginia.edu>
2021-12-05 22:04:17 -0800

    working with classification

--- ds/articles_data_analysis.R
+++ ds/articles_data_analysis.R
@@ -10,8 +10,7 @@ article_data <- read.csv("data/articles_data.csv")
 # measure of error: accuracy, precision, recall, F1
 
 # drop unneeded first index
-article_data <- article_data[, seq(2,ncol(article_data))]
-
+article_data <- article_data %>% select(-c(X))
 # how many "top" vs not articles?
 # 9161 not
 # 1276 yes
@@ -60,6 +59,7 @@ article_data %>% group_by(author) %>% summarize (count = n()) %>% nrow()
 # the sources themselves, ie, when BBC News is listed as an author
 # or (for ~ 20% of articles), normal bylines
 # either way, can't read too much into this col
+# could perhaps engineer "hasRealAuthor" as a binary
 article_data %>% 
   group_by(author) %>% 
   summarize (count = n()) %>% 
@@ -130,5 +130,270 @@ article_data %>%
 # in addition, stories that get engagement don't necessarily get read over morning coffee
 # habits are sporadic, so it's not worth splintering this
 
+# before we start to do analysis, let's randomly partition a test set that is 10% of the original data set.
+library(caret)
+trainIndex <- createDataPartition(article_data$top_article, p = .9, 
+                                  list = FALSE, 
+                                  times = 1)
+# shuffle the dataframe
+article_data <- slice(article_data, sample(1:n()))
+
+train_data <- article_data[trainIndex, ,drop=FALSE]
+test_data <- article_data[-trainIndex, ,drop=FALSE]
+
+# now, let's add features to the train data set
+
+# investigate a mentions_trump (in title) column
+train_data %>% 
+  mutate(mentions_trump = ifelse(grepl('Trump', title, ignore.case = TRUE), 1, 0)) %>%
+  group_by(mentions_trump) %>%
+  summarize(count = n(), pct = n() * 100 / nrow(train_data))
+# ~ 6% of headlines mention trump
+train_data <- train_data %>% 
+  mutate(mentions_trump = ifelse(grepl('Trump', title, ignore.case = TRUE), 1, 0)) 
+
+# let's start looking at the engagement statistics
+# engagement_plugin_comment_count is almost always 0. that one is not helpful
+train_data <- train_data %>%
+  select(-c(engagement_comment_plugin_count))
+# how many articles have "any" "engagement"?
+train_data %>%
+  filter(engagement_reaction_count != 0 | engagement_comment_count != 0 | engagement_share_count != 0) %>%
+  summarize(count = n(), pct = n() * 100 / nrow(train_data))
+
+# 3/4 have "any" engagement. good discrim
+train_data <- train_data %>%
+  mutate(engaged = ifelse(engagement_reaction_count != 0 | engagement_comment_count != 0 | engagement_share_count != 0, 1, 0))
+
+# some articles don't have "content" because they are video
+# let's make a video column that detects when video is used in the URL
+train_data %>%
+  filter(grepl('video', url, ignore.case = TRUE)) %>%
+  summarize(count = n(), pct = n() * 100 / nrow(train_data))
+
+# 7.4 % of examples
+train_data <- train_data %>%
+  mutate(video = ifelse(grepl('video', url, ignore.case = TRUE), 1, 0))
+
+# Length of the article
+# we can figure this out from the [...x chars]
+# in the content column
+# improved regex 
+library(stringr)
+train_data %>%
+  select(content) %>%
+  substr(..)
+
+# [+1744 chars]
+test_df <- as.data.frame(train_data[8493,])
+c <- test_df$content
+m <- regexec("[0-9]+ chars", c)
+regmatches(c, m)
+
+no <- "hello tim"
+z <- regexec("[0-9]+ chars", no)
+length(regmatches(no, z))
+
+test_df %>% 
+  select(content) %>% 
+  mutate(new_col = stringr::str_extract(content, "([0-9]+) chars")) %>%
+  mutate(new_col = ifelse(is.na(new_col), 0, str_sub(new_col,1,nchar(new_col)-6)))
+
+# add content_length col
+# all the abbrev are the same length, so not a big deal we don't include that part.
+train_data <- train_data %>%
+  mutate(content_length = stringr::str_extract(content, "([0-9]+) chars")) %>%
+  mutate(content_length = ifelse(is.na(content_length), 0, str_sub(content_length,1,nchar(content_length)-6))) %>%
+  mutate(content_length = as.numeric(content_length))
+
+# look at a hist of the non zero entries and non huge entries
+train_data %>% filter(content_length > 0 & content_length < 10000) %>% ggplot(aes(x = content_length)) + geom_histogram(bins = 300) 
+
+# now let's use mentions-trump, any_eng, video and char_count in logistic regression.
+# make sure those are all factors
+train_data <- train_data %>% 
+  mutate(mentions_trump = as.factor(mentions_trump)) %>%
+  mutate(engaged = as.factor(engaged)) %>%
+  mutate(video = as.factor(video)) %>% 
+  mutate(top_article = as.factor(top_article))
+
+## Logistic Regression on training data. 
+log_reg_with_four_columns <- glm(top_article ~ mentions_trump + engaged + video + content_length, data = train_data, family = "binomial")
+summary(log_reg_with_four_columns)
+
+# confidence intervals for the coefficients
+confint(log_reg_with_four_columns)
+
+library(aod)
+# chi squared for a particular column
+wald.test(b = coef(log_reg_with_four_columns), Sigma = vcov(log_reg_with_four_columns), Terms = 2)
+
+# odds ratios per coefficient
+# an article that mentions trump is 2.202 times as likely to be a top article!
+exp(coef(log_reg_with_four_columns))
+
+# deviance of the model
+with(log_reg_with_four_columns, null.deviance - deviance)
+
+# overall p val
+with(log_reg_with_four_columns, pchisq(null.deviance - deviance, df.null - df.residual, lower.tail = FALSE))
+
+library(pscl)
+# mcfadden's R2 is quite close to 0, so this isn't very good.
+pR2(log_reg_with_four_columns)
+
+# very small.
+
+# let's do some predictions.
+# first, run the test data through the same dplyr routine....
+test_data <- test_data %>%
+  mutate(mentions_trump = ifelse(grepl('Trump', title, ignore.case = TRUE), 1, 0)) %>%
+  mutate(mentions_trump = as.factor(mentions_trump)) %>%
+  select(-c(engagement_comment_plugin_count)) %>%
+  mutate(engaged = ifelse(engagement_reaction_count != 0 | engagement_comment_count != 0 | engagement_share_count != 0, 1, 0)) %>%
+  mutate(engaged = as.factor(engaged)) %>%
+  mutate(video = ifelse(grepl('video', url, ignore.case = TRUE), 1, 0)) %>%
+  mutate(video = as.factor(video)) %>%
+  mutate(content_length = stringr::str_extract(content, "([0-9]+) chars")) %>%
+  mutate(content_length = ifelse(is.na(content_length), 0, str_sub(content_length,1,nchar(content_length)-6))) %>%
+  mutate(content_length = as.numeric(content_length)) %>%
+  mutate(top_article = as.factor(top_article))
+
+test_data$top_article_p <- predict(log_reg_with_four_columns, test_data, type = "response")
+
+library(gmodels)
+# very imbalanced data set ...
+cross_table <- CrossTable(x = test_data$top_article, y = ifelse(test_data$top_article_p > .5, 1, 0), prop.chisq = FALSE, dnn = c("Actual", "Prediction"))
+
+TP <- cross_table$t[1]
+FN <- cross_table$t[2]
+FP <- cross_table$t[3]
+TN <- cross_table$t[4]
+## Recall (Sensitivity)
+(TP) / (TP + FN)
+## Specificity
+(TN) / (TN + FP)
+## Precision
+(TP) / (TP + FP)
+## False Positive Rate
+1 - ((TN) / (TN + FP))
+## Accuracy
+(TP + TN) / (TP + FP + FN + TN)
+## F1 Score
+2*TP / (2*TP + FP + FN)
+
+summary(train_data$top_article)
+
+# succesfully engineered and created a Logit model.
+# however, still running into what i saw last time
+# what to do about imbalanced data?
+# use the ROSE package.
+library(ROSE)
+# in practice, you would only want to do this to your training set
+# because you don't want to make fake test cases
+# note that in order to use this, you can only feed it
+# continuous or categorical (ie factor) vars
+# so, whatever you were feeding to the model, for instance, use that
+# so now try out oversampling + all the numerical features
+train_data_rose <- ROSE(top_article ~ mentions_trump + engagement_share_count + content_length, data =  train_data, seed = 42)$data
+
+# this is now balanced
+# lets build another model and see if it works better
+## Logistic Regression on training data. 
+rose_logit <- glm(top_article ~ mentions_trump + engagement_share_count + content_length, data = train_data_rose, family = "binomial")
+summary(rose_logit)
+pR2(rose_logit)
+
+# adding type = response directly gives us the probabilities instead of summing the coefficients
+rose_preds <- predict(rose_logit, test_data, type="response")
+
+cross_table <- CrossTable(x = test_data$top_article, y = ifelse(rose_preds > .5, 1, 0), prop.chisq = FALSE, dnn = c("Actual", "Prediction"))
+
+TP <- cross_table$t[1]
+FN <- cross_table$t[2]
+FP <- cross_table$t[3]
+TN <- cross_table$t[4]
+## Recall (Sensitivity)
+(TP) / (TP + FN)
+## Specificity
+(TN) / (TN + FP)
+## Precision
+(TP) / (TP + FP)
+## False Positive Rate
+1 - ((TN) / (TN + FP))
+## Accuracy
+(TP + TN) / (TP + FP + FN + TN)
+## F1 Score
+2*TP / (2*TP + FP + FN)
+
+varImp(rose_logit)
+
+## ROC / AUC 
+library(pROC)
+RC <- roc(test_data$top_article,ifelse(rose_preds > .5, 1, 0))
+plot(RC, legacy.axes = TRUE)
+
+## Feature Selection (for regression)
+
+# make the biggest model (all "relevant" cols)
+modelfull <- glm(top_article ~ mentions_trump + engaged + engagement_share_count + engagement_comment_count + engagement_reaction_count + video + content_length, data = train_data, family = binomial)
+# trace gets rid of output
+library(MASS)
+stepwise_model <- stepAIC(modelfull, direction = "both", trace = FALSE)
+varImp(stepwise_model)
+
+# curious, lets see if this does any better!
+
+stepwise_preds <- predict(stepwise_model, test_data, type="response")
+cross_table <- CrossTable(x = test_data$top_article, y = ifelse(stepwise_preds > .5, 1, 0), prop.chisq = FALSE, dnn = c("Actual", "Prediction"))
+TP <- cross_table$t[1]
+FN <- cross_table$t[2]
+FP <- cross_table$t[3]
+TN <- cross_table$t[4]
+## Recall (Sensitivity)
+(TP) / (TP + FN)
+## Specificity
+(TN) / (TN + FP)
+## Precision
+(TP) / (TP + FP)
+## False Positive Rate
+1 - ((TN) / (TN + FP))
+## Accuracy
+(TP + TN) / (TP + FP + FN + TN)
+## F1 Score
+2*TP / (2*TP + FP + FN)
+varImp(stepwise_model)
+library(dplyr)
+knn_train_data <- train_data %>% select(c(mentions_trump, engagement_reaction_count, engagement_comment_count, engagement_share_count, engaged, video, content_length, top_article))
+knn_test_data <- test_data %>% select(c(mentions_trump, engagement_reaction_count, engagement_comment_count, engagement_share_count, engaged, video, content_length, top_article))
+
+## lets just do KNN for practice.
+library(class)
+knnModel <- knn(train=knn_train_data, knn_test_data, cl=knn_train_data$top_article, k=2)
+summary(knnModel)
+
+cross_table <- CrossTable(x=knn_test_data$top_article,y=knnModel,prop.chisq = FALSE, dnn = c("Actual", "Prediction"))
+TP <- cross_table$t[1]
+FN <- cross_table$t[2]
+FP <- cross_table$t[3]
+TN <- cross_table$t[4]
+## Recall (Sensitivity)
+(TP) / (TP + FN)
+## Specificity
+(TN) / (TN + FP)
+## Precision
+(TP) / (TP + FP)
+## False Positive Rate
+1 - ((TN) / (TN + FP))
+## Accuracy
+(TP + TN) / (TP + FP + FN + TN)
+## F1 Score
+2*TP / (2*TP + FP + FN)
+
+knn_with_prob <- knn(train=knn_train_data, knn_test_data, cl=knn_train_data$top_article, k=2, prob = TRUE)
+probs <- attributes(knn_with_prob)[[3]]
+## new metric, log-loss
+library(MLmetrics)
+LogLoss(y_pred = probs, y_true = as.numeric(levels(test_data$top_article))[test_data$top_article])
 
 
